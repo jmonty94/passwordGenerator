6c71d2cbb574b6e8a7a6b56fdc7697758a5605c8
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-05-10 23:40:24 -0700

    first commit

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  confirm6666
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword()) 
+
+
