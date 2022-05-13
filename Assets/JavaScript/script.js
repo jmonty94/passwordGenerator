c020c502b6e7884991f2cfe0276a8ea91cf3950a
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-05-13 14:17:49 -0700

    moved variables to be declared in the global scope

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -3,41 +3,82 @@ const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  generatePassword = function() {
-    let passwordLength = parseInt(prompt("Enter a length you would like your password to be"));
-      if (passwordLength > 8 && passwordLength < 128) {
-        confirm("is " + passwordLength + " correct?")
-      }
-        else if (passwordLength < 8 || passwordLength > 128) {
-        alert("Please enter a valid whole number between 8-128")
-        }
-        else if (passwordLength !== Number) {
-        alert("Please enter a valid whole number between 8-128")
-        }
-//Asks if you would like special characters included in your new password        
-    let spChar ;
-      if (confirm("Would you like to use special characters?")) {
-        spChar = ["`~!@#$%^&*()-_=+[]{}|/?.>,<*';:"]
-      }
-        else {
-          spChar = false
-        }
-    console.log(spChar)
-    let capLtrs ;
-        if (confirm("would you like to use Capital Letters?")) {
-          capLtrs = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-        }
-          else {
-            capLtrs = false
-          }
-    console.log(capLtrs)
-  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
+const getSpChar = function() {
+  if (confirm("Would you like to use special characters?")) {
+    spChar = ["`~!@#$%^&*()-_=+[]{}|/?.>,<*';:"]
+  }
+  else {
+    spChar = false
+  }
+}
+const getCapLtrs = function() {
+  if (confirm("would you like to use Uppercase Letters?")) {
+    capLtrs = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  }
+  else {
+    capLtrs = false
+  }
+}
+const getLowLtrs = function() {
+  if (confirm("Would you like to use lowercase letter?")) {
+    lowLtrs = ["abcdefghijklmnopqrstuvwxyz"]
+  }
+  else {
+    lowLtrs = false
+  }
+}
+const getUseNum = function () {
+  if (confirm("would you like to use numbers?")) {
+    useNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  }
+  else {
+    useNum = false
+  }
+
+}
+const generatePassword = function () {
+  const passwordLength = parseInt(prompt("Enter a length you would like your password to be"));
+  let password = '';
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    confirm("is " + passwordLength + " correct?")
+    //Asks if you would like special characters included in your new password        
+    
+    const spChar = getSpChar();
+    
+    console.log(spChar)
+  
+    const capLtrs = getCapLtrs();
+    
+    console.log(capLtrs)
+  
+    const lowLtrs = getLowLtrs();
+  
+    console.log(lowLtrs)
+    
+    const useNum = getUseNum();
+   
+    console.log(useNum)
+    
+    
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    console.log(68);
+    alert("Please enter a valid whole number between 8-128");
+    // generatePassword();
+  } else if (passwordLength !== Number) {
+    console.log(71);
+    alert("Please enter a valid whole number between 8-128")
+    // generatePassword();
+  }
+  console.log("This is the pass", password);
+  return password;
+}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword);
 
 
