87035ae6d05ae0c0237f60c0af009d5fe7511ea0
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-05-12 22:51:13 -0700

    added alerts and fixed event listener

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,17 +1,26 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  generatePassword = function() {
+    let passwordLength = prompt("Enter a length you would like your password to be");
+      if (passwordLength > 8 && passwordLength < 128) {
+        confirm("is " + passwordLength + " correct?")
+      }
+        else if (passwordLength < 8 || passwordLength > 128) {
+        alert("Please enter a valid number between 8-128")
+        }
+        else if (passwordLength !== Number) {
+        alert("Please enter a valid number between 8-128")
+        }
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  confirm6666
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword()) 
+generateBtn.addEventListener("click", writePassword); 
 
 
