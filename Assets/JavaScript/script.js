f02e60fab57400ee0439678a9e662b4ab4636e04
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-05-12 22:55:56 -0700

    fixed prompt result for passwordLength value

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -4,15 +4,15 @@ const generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   generatePassword = function() {
-    let passwordLength = prompt("Enter a length you would like your password to be");
+    let passwordLength = parseInt(prompt("Enter a length you would like your password to be"));
       if (passwordLength > 8 && passwordLength < 128) {
         confirm("is " + passwordLength + " correct?")
       }
         else if (passwordLength < 8 || passwordLength > 128) {
-        alert("Please enter a valid number between 8-128")
+        alert("Please enter a valid whole number between 8-128")
         }
         else if (passwordLength !== Number) {
-        alert("Please enter a valid number between 8-128")
+        alert("Please enter a valid whole number between 8-128")
         }
   }
   var password = generatePassword();
