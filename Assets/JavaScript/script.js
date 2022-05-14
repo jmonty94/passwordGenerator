d4a39290501065f277eaba528e7c7f1481f518d8
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-05-14 15:47:58 -0700

    reworking logic

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -7,78 +7,73 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
-const getSpChar = function() {
-  if (confirm("Would you like to use special characters?")) {
-    spChar = ["`~!@#$%^&*()-_=+[]{}|/?.>,<*';:"]
+const generate = function (special,capital,lowerCase,numbers,passwordLength) {
+  let genPass =''
+  const charTypes = special + capital + lowerCase + numbers;
+  const randomFunction = {
+    special: getSpChar,
+    capital: getCapLtrs,
+    lowerCase: getLowLtrs,
+    numbers: getUseNum,
+  };
+  let charArray = [{special},{capital},{lowerCase},{numbers}].filter( item => 
+  Object.values(item)[0]);
+    if (charTypes === 0){
+      return ''
   }
-  else {
-    spChar = false
+  console.log(charArray);
+  for (let index = passwordLength; index+=charTypes;) {
+    charArray.forEach(type => {let functionNames = Object.keys(type)[0];
+    genPass += randomFunction [functionNames]()})
+    password = genPass.slice(0,passwordLength) 
+ }
+  console.log(special,capital,lowerCase,numbers,passwordLength);
+  function getSpChar () {
+   let spChar = "`~!@#$%^&*()-_=+[]{}|/?.>,<*';:"
+    return spChar[Math.floor(Math.random() * spChar.length)]
   }
-}
-const getCapLtrs = function() {
-  if (confirm("would you like to use Uppercase Letters?")) {
-    capLtrs = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  function getCapLtrs () {
+    let capLtrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return capLtrs[Math.floor(Math.random() * capLtrs.length)]}
+  function getLowLtrs () {
+    let lowLtrs = "abcdefghijklmnopqrstuvwxyz"
+    return lowLtrs[Math.floor(Math.random() * lowLtrs.length)]
   }
-  else {
-    capLtrs = false
-  }
-}
-const getLowLtrs = function() {
-  if (confirm("Would you like to use lowercase letter?")) {
-    lowLtrs = ["abcdefghijklmnopqrstuvwxyz"]
-  }
-  else {
-    lowLtrs = false
-  }
-}
-const getUseNum = function () {
-  if (confirm("would you like to use numbers?")) {
-    useNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  }
-  else {
-    useNum = false
-  }
-
+  function getUseNum () {
+      let useNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      return useNum[Math.floor(Math.random() * useNum.length)];
+    }
+    console.log(getSpChar,getCapLtrs,getLowLtrs,getUseNum);
+    getUseNum()
+ console.log(password);
+ generate(special,capital,lowerCase,numbers,passwordLength)
+ return password;
 }
 const generatePassword = function () {
   const passwordLength = parseInt(prompt("Enter a length you would like your password to be"));
-  let password = '';
+  // let password = '';
   if (passwordLength >= 8 && passwordLength <= 128) {
     confirm("is " + passwordLength + " correct?")
-    //Asks if you would like special characters included in your new password        
-    
-    const spChar = getSpChar();
-    
-    console.log(spChar)
-  
-    const capLtrs = getCapLtrs();
-    
-    console.log(capLtrs)
-  
-    const lowLtrs = getLowLtrs();
-  
-    console.log(lowLtrs)
-    
-    const useNum = getUseNum();
-   
-    console.log(useNum)
-    
-    
+    //The following variables trigger prompts        
   } else if (passwordLength < 8 || passwordLength > 128) {
     console.log(68);
     alert("Please enter a valid whole number between 8-128");
     // generatePassword();
+    return;
   } else if (passwordLength !== Number) {
     console.log(71);
-    alert("Please enter a valid whole number between 8-128")
+    alert("Please enter a valid whole number between 8-128");
     // generatePassword();
+    return;
   }
-  console.log("This is the pass", password);
+  const upperCase = window.confirm("would you like to use Uppercase Letters?");
+  const lowLtrs = window.confirm ("Would you like to use lowercase letter?");
+  const num = window.confirm ("Would you like to use numbers?");
+  const spec = window.confirm ("Would you like to use special characters?");
+  // console.log(upperCase,lowLtrs,num,spec,passwordLength);
+  // console.log("This is the pass", password);
+  generate(spec, upperCase, lowLtrs, num, passwordLength);
   return password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
