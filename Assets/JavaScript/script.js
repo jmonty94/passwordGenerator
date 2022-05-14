a6edc706fca9fb7803525dab37082279daf8bd80
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-05-14 16:05:39 -0700

    Creates and displays password but in same pattern every time

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -7,8 +7,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-const generate = function (special,capital,lowerCase,numbers,passwordLength) {
-  let genPass =''
+const generate = function (special, capital, lowerCase, numbers, passwordLength) {
+  let genPass = ''
   const charTypes = special + capital + lowerCase + numbers;
   const randomFunction = {
     special: getSpChar,
@@ -16,38 +16,42 @@ const generate = function (special,capital,lowerCase,numbers,passwordLength) {
     lowerCase: getLowLtrs,
     numbers: getUseNum,
   };
-  let charArray = [{special},{capital},{lowerCase},{numbers}].filter( item => 
-  Object.values(item)[0]);
-    if (charTypes === 0){
-      return ''
+  let charArray = [{ special }, { capital }, { lowerCase }, { numbers }].filter(item =>
+    Object.values(item)[0]);
+  if (charTypes === 0) {
+    return ''
   }
   console.log(charArray);
-  for (let index = passwordLength; index+=charTypes;) {
-    charArray.forEach(type => {let functionNames = Object.keys(type)[0];
-    genPass += randomFunction [functionNames]()})
-    password = genPass.slice(0,passwordLength) 
- }
-  console.log(special,capital,lowerCase,numbers,passwordLength);
-  function getSpChar () {
-   let spChar = "`~!@#$%^&*()-_=+[]{}|/?.>,<*';:"
+  for (let index =0; index < passwordLength; index += charTypes) {
+    charArray.forEach(type => {
+      let functionNames = Object.keys(type)[0];
+      genPass += randomFunction[functionNames]()
+    })
+    password = genPass.slice(0, passwordLength)
+    console.log(password);
+  }
+  console.log(special, capital, lowerCase, numbers, passwordLength);
+  function getSpChar() {
+    let spChar = "`~!@#$%^&*()-_=+[]{}|/?.>,<*';:"
     return spChar[Math.floor(Math.random() * spChar.length)]
   }
-  function getCapLtrs () {
+  function getCapLtrs() {
     let capLtrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    return capLtrs[Math.floor(Math.random() * capLtrs.length)]}
-  function getLowLtrs () {
+    return capLtrs[Math.floor(Math.random() * capLtrs.length)]
+  }
+  function getLowLtrs() {
     let lowLtrs = "abcdefghijklmnopqrstuvwxyz"
     return lowLtrs[Math.floor(Math.random() * lowLtrs.length)]
   }
-  function getUseNum () {
-      let useNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-      return useNum[Math.floor(Math.random() * useNum.length)];
-    }
-    console.log(getSpChar,getCapLtrs,getLowLtrs,getUseNum);
-    getUseNum()
- console.log(password);
- generate(special,capital,lowerCase,numbers,passwordLength)
- return password;
+  function getUseNum() {
+    let useNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return useNum[Math.floor(Math.random() * useNum.length)];
+  }
+  // console.log(getSpChar, getCapLtrs, getLowLtrs, getUseNum);
+  // getUseNum()
+  // console.log(password);
+  // generate(special, capital, lowerCase, numbers, passwordLength)
+  return password;
 }
 const generatePassword = function () {
   const passwordLength = parseInt(prompt("Enter a length you would like your password to be"));
@@ -67,9 +71,9 @@ const generatePassword = function () {
     return;
   }
   const upperCase = window.confirm("would you like to use Uppercase Letters?");
-  const lowLtrs = window.confirm ("Would you like to use lowercase letter?");
-  const num = window.confirm ("Would you like to use numbers?");
-  const spec = window.confirm ("Would you like to use special characters?");
+  const lowLtrs = window.confirm("Would you like to use lowercase letter?");
+  const num = window.confirm("Would you like to use numbers?");
+  const spec = window.confirm("Would you like to use special characters?");
   // console.log(upperCase,lowLtrs,num,spec,passwordLength);
   // console.log("This is the pass", password);
   generate(spec, upperCase, lowLtrs, num, passwordLength);
